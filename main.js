f177e37a197a67235c2d65854059a60167258c48
douglas amport <douglas.amport@gmail.com>
2021-10-28 06:42:03 -0400

    updated Image to randomzie

--- main.js
+++ main.js
@@ -6,18 +6,15 @@ const contentArr = $('#content')                            // Collect primary e
 const content = contentArr[0]
 const contentNext = content.nextElementSibling              // Set next element
 const contentParent = content.parentNode                    // Set parent element,  Happens to be undefined
-console.log('Parent', contentParent)
-console.log('Next', contentNext)
 content.remove()                                            // remove elemetn from DOM, Jquery syntax
 
-
 //-----Set Image and insert it in DOM----------------
-const focusImage = document.createElement('img')           
-focusImage.src = chrome.runtime.getURL("img/focus.jpg");    // Get full URL of Image in chrome_estenstions:// etc.... dont forget to allow in manifest.json
+const focusImage = document.createElement('img');           
+focusImage.src = "https://source.unsplash.com/random/?space"//get random image from unsplash.com
+// focusImage.src = chrome.runtime.getURL("img/focus.jpg"); // Get full URL of Image in chrome_estenstions:// etc.... dont forget to allow in manifest.json
 focusImage.alt = "green road sign that reads 'focus' "      // Include ALT text for screen readers
 $(focusImage).css({"position":"absolute", "left":"50%", "transform":"translate(-50%, 0)"});  
 contentNext.prepend(focusImage);                            // Add image to DOM
-
 
 //-----Wrap Image in div to prep for msg  ----------------
 let focusWrapper = document.createElement('div')            // Create new div element
@@ -34,17 +31,14 @@ $(".focusWrapper").wrap(focusContainer)                     //wrap image div in 
 function chooseWord(time) {
 return (time >= 8 && time < 20) ? 'studying' : 'sleeping'
 }
-
-let word = chooseWord(hour)
+let word = chooseWord(hour);
 
 //-----Create msg and Prepend into container  ----------------
 message = document.createElement('div')                     // Create new div element
 message.innerHTML = `Shouldn\'t you be ${word}?`           // Add text to div
 message.className = 'focusMessage'                          // Apply class focusMessage
-$(message).css({"font-size":"5em","position":"absolute", "top":"1.5em", "left":"50%", "transform":"translate(-50%, 0)", "z-index":"1", "white-space":"nowrap"})  // Set CSS, include position absolute  , "white-space":"nowrap"
+$(message).css({ "color":"white", "font-size":"5em","position":"absolute", "top":"1.5em", "left":"50%", "transform":"translate(-50%, 0)", "z-index":"1", "white-space":"nowrap"})  // Set CSS, include position absolute  , "white-space":"nowrap"
 $(".focusContainer").prepend(message);                      //prepend message as first child of focusContainer
-
-
 
 //----recieve msg from popup to remove image and reset content ------------
 chrome.runtime.onMessage.addListener(function(request) {
@@ -55,25 +49,3 @@ chrome.runtime.onMessage.addListener(function(request) {
         contentNext.prepend(content);                    // prepend content before contentNext
     }
 });
-
-
-//$.ajax({
-//    method: 'GET',
-//    url: 'https://api.unsplash.com/',
-//    success: function(result) {
-//     // result is whatever the URL sends back from the request
-//     console.logI(result)
-//    },
-//    error: function(err) {
-//     console.log(err)
-//     // if any errors occur during the process you can check out the
-//     // the error by logging the 'err' argument
-//    }
-//  });
-//
-//console.log(focusWrapper)
-
-
-
-
-
